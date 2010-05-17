2ed8e7e005981b8e99c4bc91131aaf847ef3429a
catena83 <catena83@2235845c-279c-11df-a6dc-d9c9baf28c7f>
2010-05-17 15:00:13 +0000

    Implementate le funzioni di indicizzazione e di search. Aggiunta la classe ComparatoreDocumentiBean per ordinare i risultati.

--- motorericercaJava/src/persistence/MyAnalyzer.java
+++ motorericercaJava/src/persistence/MyAnalyzer.java
@@ -5,9 +5,17 @@
 
 package persistence;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
+import java.util.HashSet;
+import java.util.Set;
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.LowerCaseFilter;
+import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.util.Version;
 
@@ -21,7 +29,26 @@ public class MyAnalyzer extends Analyzer{
     public TokenStream tokenStream(String string, Reader reader) {
         TokenStream result = new StandardTokenizer(Version.LUCENE_30, reader);
 
+        result = new StandardFilter(result);
+        result = new LowerCaseFilter(result);
+        result = new StopFilter(true, result, getStopWords());
+
         return result;
+    }
+
+    private static Set<String> getStopWords(){
+        Set<String> stringSet = new HashSet<String>();
+
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("config/stopWord"));
+            String str;
+            while ((str = in.readLine()) != null) {
+		stringSet.add(str);
+            }
+            in.close();
+        } catch (IOException e) {
+        }
+        return stringSet;
     }
 
 }
