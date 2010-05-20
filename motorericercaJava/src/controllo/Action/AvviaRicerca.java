11c15a651374ed76f87ee3c93e2a58a19295616d
catena83 <catena83@2235845c-279c-11df-a6dc-d9c9baf28c7f>
2010-05-20 18:31:05 +0000

    Implementata l'interfaccia web (funziona)

--- motorericercaJava/src/controllo/Action/AvviaRicerca.java
+++ motorericercaJava/src/controllo/Action/AvviaRicerca.java
@@ -7,8 +7,10 @@ package controllo.Action;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.Timer;
 import javax.servlet.http.HttpServletRequest;
+import modello.DocumentoBean;
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.store.LockObtainFailedException;
@@ -20,17 +22,19 @@ import persistence.IndexFunction;
  */
 public class AvviaRicerca {
 
-    public boolean avviaAzione(HttpServletRequest request) throws CorruptIndexException, LockObtainFailedException, IOException, ParseException {
+    public LinkedList<DocumentoBean> avviaAzione(HttpServletRequest request) throws CorruptIndexException, LockObtainFailedException, IOException, ParseException {
 
-        IndexThread it = new IndexThread();
-        Timer t = new Timer(true);
-        t.scheduleAtFixedRate(it, new Date (System.currentTimeMillis()), 10000);
+//        IndexThread it = new IndexThread();
+//        Timer t = new Timer(true);
+//        t.scheduleAtFixedRate(it, new Date (System.currentTimeMillis()), 168750);
 
 
         IndexFunction index = new IndexFunction();
 
         //ritorna false se la lista è piena
-        return index.search(request.getParameter("search"));
+        index.search(request.getParameter("search"));
+
+        return index.getDocumentList();
 
     }
 
